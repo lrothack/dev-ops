c7f2a4255414a0b4d8d5bc1ec7278376c89f9a82
Leonard Rothacker <leonard.rothacker@googlemail.com>
2020-06-10 18:53:44 +0200

    Parsing version string from __init__

--- sampleproject/__init__.py
+++ sampleproject/__init__.py
@@ -5,3 +5,5 @@ LOGGING_FORMAT = '%(asctime)-15s: [%(name)s] %(message)s'
 # LOGGING_FORMAT = '[%(name)s] %(message)s'
 logging.basicConfig(level=logging.INFO,
                     format=LOGGING_FORMAT)
+
+__version__ = '0.1.0'

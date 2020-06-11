d7b172cf942fee4e9d25d190db544039a529eeb0
Leonard Rothacker <leonard.rothacker@googlemail.com>
2020-06-11 02:30:21 +0200

    Using template for project_version

--- {{project_slug}}/__init__.py
+++ {{project_slug}}/__init__.py
@@ -3,6 +3,6 @@ from {{project_slug}}.log import LoggerConfig
 
 
 # Package meta information
-__version__ = '0.1.0'
+__version__ = '{{project_version}}'
 # Logger configuration can be accessed through global variable LOGCONFIG
 LOGCONFIG = LoggerConfig()
